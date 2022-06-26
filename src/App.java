df3b4d2ab6fbbb49aa8b544436ce8c67d319e294
Julio Olmedo <turlan125@gmail.com>
2022-06-25 20:49:56 -0400

    tema 4

--- src/App.java
+++ src/App.java
@@ -3,36 +3,36 @@ public class App {
 
         // ejercicio del IF
         int numeroIf = -100;
-        if(numeroIf>0){
-            System.out.println(numeroIf+" es positivo");
-        }else if(numeroIf < 0){
-            System.out.println(numeroIf+" es negativo");
-        }else {
-            System.out.println(numeroIf+" es 0");
+        if (numeroIf > 0) {
+            System.out.println(numeroIf + " es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println(numeroIf + " es negativo");
+        } else {
+            System.out.println(numeroIf + " es 0");
         }
 
         // ejercicio del WHILE
         int numeroWhile = -3;
-        while(numeroWhile < 3){
+        while (numeroWhile < 3) {
             numeroWhile++;
             System.out.println(numeroWhile);
         }
 
         // ejercicio del DO WHILE
-        do{
+        do {
             numeroWhile++;
             System.out.println(numeroWhile);
-        }while(numeroWhile < 3);
+        } while (numeroWhile < 3);
 
         // ejercicio del FOR
-        for(int numeroFor =0; numeroFor <= 3; numeroFor++){
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
             System.out.println(numeroFor);
         }
 
         // ejercicio del SWITCH
-        
+
         String estacion = "INVIERNO";
-        switch(estacion){
+        switch (estacion) {
             case "OTONO":
                 System.out.println("Es otonio");
                 break;
@@ -48,6 +48,5 @@ public class App {
             default:
                 System.out.println("No es una estacion");
         }
-
     }
 }
